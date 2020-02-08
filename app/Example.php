b319f9b55a5a7c39e1bc7e1cf9e616858b503ee1
TimB1990 <t.boons@live.nl>
2020-02-08 14:31:53 +0100

    futher container test

--- app/Example.php
+++ app/Example.php
@@ -4,13 +4,8 @@ namespace App;
 
 class Example
 {
-
-    protected $collaborator;
-    protected $foo;
-
-    public function __construct(Collaborator $collaborator, $foo)
+    public function handle()
     {
-        $this->collaborator = $collaborator;
-        $this->foo = $foo;
+        die('it works');
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,32 +1,21 @@
 <?php
 
 namespace App\Providers;
+use App\Example;
 
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        $this->app->bind('App\Example', function () {
+        // put key in container
+        $this->app->bind('example', function () {
+            return new Example();
+        });
 
-            $collaborator = new \App\Collaborator();
-            $foo = 'foobar';
-
-            return new \App\Example($collaborator, $foo); 
-         });
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         //
